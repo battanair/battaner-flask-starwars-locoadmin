a54b004e9e2d9cb83d3fe81a96f74acb9e445fcc
Eduardo Battaner <battanedd@gmail.com>
2025-03-18 12:31:45 +0000

    redo

--- src/app.py
+++ src/app.py
@@ -2,14 +2,21 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+import bcrypt
+from datetime import timedelta
+
+from utils import APIException, generate_sitemap
+from admin import setup_admin
+
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
-#from models import Person
+from sqlalchemy import or_
+
+from models import db, Users,Favorites,Films,Planets,People,FavoritesType
+from flask_jwt_extended import create_access_token,get_csrf_token,get_jwt_identity,jwt_required,JWTManager,set_access_cookies,unset_jwt_cookies
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -21,9 +28,23 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+#JWT
+jwt_key = os.getenv("JWT_SECRET_KEY")
+
+app.config["JWT_SECRET_KEY"] = "cO48sKPDnc3cbbnAqwq"  # Change this!
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_COOKIE_CSRF_PROTECT"] = True
+app.config["JWT_CSRF_IN_COOKIES"] = True
+app.config["JWT_COOKIE_SECURE"] = True
+app.config["JWT_ACCESS_COOKIE_NAME"] = "access_token_cookie"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_ACCESS_CSRF_HEADER_NAME"] = "X-CSRF-TOKEN" 
+jwt = JWTManager(app)
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
-CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app,supports_credentials=True)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
@@ -36,14 +57,310 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+tables = {
+        "films" : Films,
+    "planets" : Planets,
+    "people" : People
     }
 
-    return jsonify(response_body), 200
+
+@app.route('/register',methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get("email")
+    username = data.get("username")
+    password = data.get("password")
+
+    required_fields = ["username","email","password"]
+    if not all(field in data for field in required_fields):
+        return jsonify({"message":"Missing required fields"}),400
+    
+    existing_user = db.session.query(Users).filter(or_(Users.username == username,Users.email == email)).first()
+    if existing_user:
+        return jsonify({"error":"Username or Email already registered"}),400
+    
+    hashedPassword = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+
+    new_user = Users(email=email,username=username,password=hashedPassword)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message":"User registered successfully"}),201
+
+
+@app.route('/login', methods=['POST'])
+def get_login():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+
+    required_fields= ["username","password"]
+
+    missing_fields = [field for field in required_fields if field not in data]
+    if missing_fields:
+        return jsonify({"error": "Missing required fields", "missing": missing_fields}), 400
+    
+    user = Users.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({"error":"Username not found"}), 400
+
+    is_password_valid = bcrypt.checkpw(password.encode('utf-8'),user.password.encode('utf-8'))
+
+    if not is_password_valid:
+        return jsonify({"error":"Password not correct"}), 400
+
+    access_token = create_access_token(identity=str(user.user_id))
+    csrf_token = get_csrf_token(access_token)
+    response = jsonify({
+        "msg": "login successful",
+        "user": {
+            "id": user.user_id,
+            "username": user.username
+        },
+        "csrf_token": csrf_token
+        })
+    
+    set_access_cookies(response,access_token)
+    
+    return response
+
+@app.route('/me', methods=['GET'])
+@jwt_required()
+def get_current_user():
+    user_id = get_jwt_identity()
+    user = Users.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+    response = jsonify({
+        "id": user.user_id,
+        "username": user.username
+    })
+
+    return response,200
+
+
+@app.route('/logout',methods=['POST'])
+@jwt_required()
+def logout_with_cookies():
+    response= jsonify({"message":"logout successful"})
+    unset_jwt_cookies(response)
+    return response
+    
+                                            #GET USERS
+
+@app.route('/users',methods=['GET'])
+def get_user():
+    users = Users.query.all()
+    response_body = {
+        "content": users
+    }
+    return jsonify(response_body),200
+
+                                            # GET,POST & DELETE FAVORITES
+
+@app.route('/favorites', methods=["GET", "POST", "DELETE"])
+@jwt_required()
+def handle_favorites():
+    user_id = get_jwt_identity()
+    if request.method == "GET":
+        favorites = Favorites.query.filter_by(user_id=user_id).all()
+        return jsonify(favorites), 200
+
+    data = request.get_json()
+
+    if request.method == "POST":
+        required_fields = ["type_enum","external_id","name"]
+    
+        if not all(field in data for field in required_fields):
+            return jsonify("error: missing required fields"), 400
+        if data["type_enum"] not in FavoritesType.__members__:
+            return jsonify({"error": "Invalid type_enum. Must be one of valid types"}), 400
+        if Favorites.query.filter_by(external_id=data["external_id"],user_id=user_id,type_enum=data["type_enum"]).first():
+            return jsonify({"error":"Resources already in favorites"}), 400
+        if not tables[data["type_enum"]].query.filter_by(id=data["external_id"]).first():
+            return jsonify({"error":"Resource not found"}), 400
+        
+        new_favorite = Favorites(
+            user_id=user_id,
+            external_id=data["external_id"],
+            name=data["name"],
+            type_enum=data["type_enum"]
+        )
+
+        db.session.add(new_favorite)
+        db.session.commit()
+        return jsonify(new_favorite), 200
+
+
+    if request.method == "DELETE":
+        required_fields = ["favorite_id"]
+        
+        missing_fields = [field for field in required_fields if field not in data]
+        if missing_fields:
+            return jsonify({"error": "Missing required fields", "missing": missing_fields}), 400
+
+
+        favorite = Favorites.query.filter_by(
+            favorite_id=data["favorite_id"],user_id=user_id
+        ).first()
+
+        if not favorite:
+            return jsonify({"error":"Favorite not found"}), 400
+    
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({"message":"Favorite deleted"}), 200
+
+                                            # GET,POST & DELETE FILMS
+
+@app.route('/films',methods=['GET'])
+def get_films():
+    films_list = Films.query.all()
+    response_body = {
+        "content": films_list
+    }
+    return jsonify(response_body),200
+
+@app.route('/films/<int:id>',methods=['GET'])
+def get_film(id):
+    film = Films.query.get(id)
+    response_body = {
+        "content": film
+    }
+    return jsonify(response_body),200
+
+@app.route('/films',methods=['POST'])
+def post_film():
+    data = request.get_json(force=True)
+    required_fields = ["title","episode","release_date","director","producer"]
+    missing_fields = [field for field in required_fields if field not in data]
+    if missing_fields:
+        return ({"message":"error","missing fields" : missing_fields}),400
+
+    new_film = Films(
+        name=data["name"],
+        episode=data["episode"],
+        release_date=data["release_date"],
+        opening_crawl=data["opening_crawl"],
+        director=data["director"],
+        producer=data["producer"],
+    )
+    db.session.add(new_film)
+    db.session.commit()
+    return jsonify(new_film),200
+
+@app.route('/films/<int:id>',methods=['DELETE'])
+def delete_film(id):
+    film = Films.query.get(id)
+    if not film:
+        return jsonify({"message":"No film found with the requested id"}),400
+
+    db.session.delete(film)
+    db.session.commit()
+
+    return jsonify({"message": "Film deleted successfully"}),200
+
+                                         # GET,POST & DELETE PLANETS
+
+@app.route('/planets',methods=['GET'])
+def get_planets():
+    planets_list = Planets.query.all()
+    response_body = {
+        "content": planets_list
+    }
+    return jsonify(response_body),200
+
+@app.route('/planets/<int:id>',methods=['GET'])
+def get_planet(id):
+    planet = Planets.query.get(id)
+    response_body = {
+        "content": planet
+    }
+    return jsonify(response_body),200   
+
+@app.route('/planets',methods=['POST'])
+def post_planet():
+    data = request.get_json(force=True)
+    required_fields = ["name","population","climate","diameter","gravity"]
+    missing_fields = [field for field in required_fields if field not in data]
+    if missing_fields:
+        return ({"message":"error","missing fields" : missing_fields}),400
+
+    new_planet = Planets(
+        name=data["name"],
+        population=data["population"],
+        climate=data["climate"],
+        diameter=data["diameter"],
+        gravity=data["gravity"],
+    )
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify(new_planet),200
+
+@app.route('/planets/<int:id>',methods=['DELETE'])
+def delete_planet(id):
+    planet = Planets.query.get(id)
+    if not planet:
+        return jsonify({"message":"No planet found with the requested id"}),400
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return jsonify({"message": "Planet deleted successfully"}),200
+
+
+                                            # GET,POST & DELETE PEOPLE
+@app.route('/people',methods=['GET'])
+def get_people():
+    people_list = People.query.all()
+    response_body = {
+        "content": people_list
+    }
+
+    return jsonify(response_body),200
+
+@app.route('/people/<int:id>',methods=['GET'])
+def get_person(id):
+    person = People.query.get(id)
+    response_body = {
+        "content": person
+    }
+    return jsonify(response_body),200   
+
+@app.route('/people',methods=['POST'])
+def post_person():
+    data = request.get_json(force=True)
+    required_fields = ["name","species","skin_color","hair_color","height","homeworld"]
+    missing_fields = [field for field in required_fields if field not in data]
+    if missing_fields:
+        return ({"message":"error","missing fields" : missing_fields}),400
+
+    new_person = People(
+        name=data["name"],
+        species=data["species"],
+        skin_color=data["skin_color"],
+        hair_color=data["hair_color"],
+        height=data["height"],
+        homeworld=data["homeworld"]
+    )
+    db.session.add(new_person)
+    db.session.commit()
+    return jsonify(new_person),200
+
+@app.route('/person/<int:id>',methods=['DELETE'])
+def delete_person(id):
+    person = People.query.get(id)
+    if not person:
+        return jsonify({"message":"No person found with the requested id"}),400
+
+    db.session.delete(person)
+    db.session.commit()
+
+    return jsonify({"message": "Person deleted successfully"}),200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,70 @@
+import enum
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey, Integer, String
+from dataclasses import dataclass,field
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+@dataclass
+class Users(db.Model):
+    __tablename__ = 'users'
+    user_id:int = db.Column(db.Integer, primary_key=True)
+    email:str = db.Column(db.String(120), unique=True, nullable=False)
+    username:str = db.Column(db.String(50), nullable=False,unique=True)
+    password = db.Column(db.VARCHAR(60), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Users %r>' % self.username
 
-    def serialize(self):
+    """def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
-        }
+        }"""
+    
+class FavoritesType(str,enum.Enum):
+    films = "films"
+    planets = "planets"
+    people = "people"
+
+@dataclass
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    favorite_id:int = db.Column(db.Integer,unique=True, primary_key=True,index=True)
+    user_id:int = db.Column(db.Integer, ForeignKey('users.user_id'),nullable=False)
+    external_id:int = db.Column(db.Integer,nullable=False)
+    name:str = db.Column(db.String(50), unique=True,nullable=False)
+    type_enum: FavoritesType = db.Column(db.Enum(FavoritesType), nullable=False)
+
+@dataclass
+class Films(db.Model):
+    __tablename__ = 'films'
+    id:int = db.Column(db.Integer,primary_key=True,unique=True)
+    name:str = db.Column(db.String(50),nullable=False,unique=True)
+    episode:int = db.Column(db.Integer,nullable=False,unique=True)
+    release_date:int = db.Column(db.Integer,nullable=False)
+    opening_crawl:int = db.Column(db.String(500),nullable=False)
+    director:str = db.Column(db.String(50),nullable=False)
+    producer:str = db.Column(db.String(50),nullable=False)
+
+@dataclass
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id:int = db.Column(db.Integer,primary_key=True,unique=True)
+    name:str = db.Column(db.String(50),nullable=False,unique=True)
+    population:int = db.Column(db.Integer,nullable=False)
+    climate:str = db.Column(db.String(50),nullable=False)
+    diameter:str = db.Column(db.String(50),nullable=False)
+    gravity:int = db.Column(db.Integer,nullable=False)
+
+@dataclass
+class People(db.Model):
+    __tablename__ = 'people'
+    id:int = db.Column(db.Integer,primary_key=True,unique=True)
+    name:str = db.Column(db.String(50),unique=True,nullable=False)
+    species:str = db.Column(db.String(50),nullable=False)
+    skin_color:str = db.Column(db.String(50),nullable=False)
+    hair_color:str = db.Column(db.String(50),nullable=False)
+    height:int = db.Column(db.Integer,nullable=False)
+    homeworld:int = db.Column(db.Integer,ForeignKey('planets.id'),nullable=False)
